7bd86d65c0990094eeea41276e2e8d799f10c0eb
Philip T <69321457+philipscoderepo@users.noreply.github.com>
2023-03-09 21:56:50 -0500

    add to service

--- article-service/index.js
+++ article-service/index.js
@@ -11,6 +11,16 @@ app.get('/', function (req, res) {
     res.json(data);
 });
 
+app.get('/article', function(req, res){
+    data.articles.forEach(article => {
+        console.log(req.query.id)
+        if(article.article_id == req.query.id){
+            res.send(article);
+        }
+    })
+    res.status(404).send();
+})
+
 app.listen(3001, () => {
     console.log("Server is listening on port 3001...");
 });
